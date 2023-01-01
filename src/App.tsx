ab1d192e4c9c321de4c0f97d440f8241803f5e45
Andrew Koch <andrewkoch@andrews-mbp-2.lan>
2022-12-31 19:44:43 -0800

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import {ThreeTemplate} from './ThreeTemplate';
+
+function App() {
+  return (
+    <div className="App">
+        <ThreeTemplate />
+    </div>
+  );
+}
+
+export default App;
